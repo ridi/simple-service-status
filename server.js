ba3e53e8c9c94e075245b704b23502489e7c89bf
kyungmi.k <kyungmi.k@ridi.com>
2016-12-16 20:00:35 +0900

    test

--- server.js
+++ server.js
@@ -1,22 +1,64 @@
+
 'use strict';
 
 const Hapi = require('hapi');
+const Joi = require('joi');
+const co = require('co');
+const Status = require('./lib/Status');
+const mongorito = require('mongorito');
 
-// Create a server with a host and port
 const server = new Hapi.Server();
-server.connection({
-    port: ~~process.env.PORT || 8000
-});
+server.connection({ port: ~~process.env.PORT || 8000 });
 
-// Add the route
-server.route({
-    method: 'GET',
-    path:'/hello',
-    handler: function (request, reply) {
-
-        return reply('hello world');
+let handlers = {
+    getStatus: (request, reply) => {
+        Status
+            .get(request.params.deviceType, request.params.deviceVersion, request.params.appVersion)
+            .then(function (result) {
+                console.log(result);
+                reply(result);
+            }).catch(function (err) {
+                reply(err);
+            });
+    },
+    addStatus: (request, reply) => {
+        Status
+            .save(request.params)
+            .then(function (result) {
+                reply(result);
+            }).catch(function (err) {
+                reply(err);
+            });
+    },
+    test: (request, reply) => {
+        Status
+            .test()
+            .then(function (result) {
+                reply(result);
+            }).catch(function (err) {
+            reply(err);
+        });
     }
-});
+};
+
+let validators = {
+    checkStatus: {
+        query: {
+            deviceType: Joi.string().required(),
+            deviceVersion: Joi.string().regex(/^[0-9\.\*]+$/).required(),
+            appVersion: Joi.string().regex(/^[0-9\.\*]+$/).required()
+        }
+    },
+    addStatus: {
+        params: {
+
+        }
+    }
+};
+
+server.route({method: 'GET', path: '/status', handler: handlers.getStatus, config: {validate: validators.getStatus}});
+server.route({method: 'GET', path: '/test', handler: handlers.test});
+server.route({method: 'POST', path: '/status', handler: handlers.addStatus, config: {validate: validators.addStatus}});
 
 // Start the server
 server.start((err) => {
