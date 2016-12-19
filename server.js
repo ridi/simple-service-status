b0277795bfe8c4cd28956cc0883cf8a963be981c
kyungmi.k <kyungmi.k@ridi.com>
2016-12-19 19:16:04 +0900

    add auth plugin

--- server.js
+++ server.js
@@ -2,68 +2,61 @@
  * Service API router
  *
  * @since 1.0.0
+ *
+ * // TODO 1 minute caching (query=>key)
+ * // TODO findAll, save => authentication
  */
 
 'use strict';
 
 const Hapi = require('hapi');
-const Joi = require('joi');
-const Status = require('./lib/Status');
+const hapiAuthCookie = require('hapi-auth-cookie');
+
+const apiRouter = require('./lib/server-api');
+const baseRouter = require('./lib/server-auth');
 
 const server = new Hapi.Server();
 server.connection({ port: process.env.PORT || 8080 });
 
-let handlers = {
-    getStatus: (request, reply) => {
-        Status.find(request.query.deviceType, request.query.deviceVersion, request.query.appVersion)
-            .then(function (result) {
-                console.log(result);
-                reply(result);
-            }).catch(function (err) {
-                reply(err);
-            });
-    },
-    addStatus: (request, reply) => {
-        Status.save(request.params)
-            .then(function (result) {
-                reply(result);
-            }).catch(function (err) {
-                reply(err);
-            });
-    },
-    test: (request, reply) => {
-        Status.test()
-            .then(function (result) {
-                reply(result);
-            }).catch(function (err) {
-            reply(err);
-        });
-    }
-};
-
-let validators = {
-    checkStatus: {
-        query: {
-            deviceType: Joi.string().required(),
-            deviceVersion: Joi.string().regex(/^[0-9\.\*]+$/).required(),
-            appVersion: Joi.string().regex(/^[0-9\.\*]+$/).required()
-        }
-    },
-    addStatus: {
-        params: {
-
-        }
-    }
-};
-
-server.route({method: 'GET', path: '/status', handler: handlers.getStatus, config: {validate: validators.getStatus}});
-server.route({method: 'GET', path: '/test', handler: handlers.test});
-server.route({method: 'POST', path: '/status', handler: handlers.addStatus, config: {validate: validators.addStatus}});
-
-// Start the server
-server.start((err) => {
+server.register(hapiAuthCookie, (err) => {
     if (err) {
         throw err;
     }
-    console.log('Server running at:', server.info.uri);
+
+    const cache = server.cache({ segment: 'sessions', expiresIn: 3 * 24 * 60 * 60 * 1000 });
+    server.app.cache = cache;
+
+    server.auth.strategy('session', 'cookie', true, {
+        password: 'password-should-be-32-characters',
+        cookie: 'sid-example',
+        redirectTo: '/login',
+        isSecure: false,
+        validateFunc: function (request, session, callback) {
+
+            cache.get(session.sid, (err, cached) => {
+
+                if (err) {
+                    return callback(err, false);
+                }
+
+                if (!cached) {
+                    return callback(null, false);
+                }
+
+                return callback(null, true, cached.account);
+            });
+        }
+    });
+
+    server.route(apiRouter);
+    server.route(baseRouter);
+
+    // Start the server
+    server.start((err) => {
+        if (err) {
+            throw err;
+        }
+        console.log('Server running at:', server.info.uri);
+    });
 });
+
