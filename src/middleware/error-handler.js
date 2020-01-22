ea2cdab09fd4ddb2c8a9b3e0f53d106cc5789925
kering <kering@ridi.com>
2020-01-22 21:01:23 +0900

    Call `takeover()` to API error response.

--- src/middleware/error-handler.js
+++ src/middleware/error-handler.js
@@ -17,8 +17,6 @@ const generateErrorResponseBody = request => ({
   success: false,
 });
 
-const getStatusCode = request => request.response.statusCode || request.response.output.statusCode;
-
 /* eslint no-param-reassign: ["error", { "props": false }] */
 const onSuccess = (request, h, options) => {
   const { path, response } = request;
@@ -30,31 +28,30 @@ const onSuccess = (request, h, options) => {
   return null;
 };
 
-const onApiError = (request, h) => {
-  const statusCode = getStatusCode(request);
+const onApiError = (request, h, statusCode) => {
   const responseBody = generateErrorResponseBody(request);
-  switch (statusCode) {
-    case 400:
-      if (request.response.data && request.response.data.name === 'ValidationError') {
-        responseBody.code = SSSError.Types.BAD_REQUEST_INVALID.code;
-        responseBody.message = SSSError.Types.BAD_REQUEST_INVALID.message({ message: request.response.message });
-      }
-      return h.response(responseBody).code(statusCode);
-    case 401:
-    case 403:
-      return h.response(responseBody).unstate('token').code(statusCode);
-    case 500:
-      if (!responseBody.code) {
-        responseBody.code = SSSError.Types.SERVER.code;
-      }
-      return h.response(responseBody).code(statusCode);
-    default:
-      return h.response(responseBody).code(statusCode);
-  }
+  return (() => {
+    switch (statusCode) {
+      case 400:
+        if (request.response.data && request.response.data.name === 'ValidationError') {
+          responseBody.code = SSSError.Types.BAD_REQUEST_INVALID.code;
+          responseBody.message = SSSError.Types.BAD_REQUEST_INVALID.message({ message: request.response.message });
+        }
+        return h.response(responseBody);
+      case 401:
+      case 403:
+        return h.response(responseBody).unstate('token');
+      case 500:
+        if (!responseBody.code) {
+          responseBody.code = SSSError.Types.SERVER.code;
+        }
+      default:
+        return h.response(responseBody);
+    }
+  })().code(statusCode).takeover();
 };
 
-const onUIError = (request, h, options) => {
-  const statusCode = getStatusCode(request);
+const onUIError = (request, h, options, statusCode) => {
   const responseBody = generateErrorResponseBody(request);
   switch (statusCode) {
     case 401:
@@ -69,25 +66,17 @@ const onUIError = (request, h, options) => {
   }
 };
 
-const onError = (request, h, options) => {
+const onError = (request, h, options, statusCode) => {
   const isApi = request.path.includes(options.apiPrefix);
-  return isApi
-    ? onApiError(request, h)
-    : onUIError(request, h, options);
+  return isApi ? onApiError(request, h, statusCode) : onUIError(request, h, options, statusCode);
 };
 
 const register = (server, opts) => {
   const options = Object.assign({}, defaultOptions, opts);
   server.ext('onPreResponse', (request, h) => {
     const statusCode = request.response.statusCode || request.response.output.statusCode;
-    const response = (statusCode === 200)
-      ? onSuccess(request, h, options)
-      : onError(request, h, options);
-
-    if (response) {
-      return response;
-    }
-    return h.continue;
+    const response = (statusCode === 200) ? onSuccess(request, h, options) : onError(request, h, options, statusCode);
+    return response || h.continue;
   });
 };
 
